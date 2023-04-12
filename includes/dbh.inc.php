c2be493bbbd0e22797ad04cff2a55f6dae2c567a
Johnny Castano <88343685+JohnnyCastano@users.noreply.github.com>
2023-04-12 17:36:28 -0500

    update

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "datadev.devcatalyst.com";
+$dbUsername = "mahs_Codephobia";
+$dbPassword = "mustangs";
+$dbName = "mahs_Codephobia";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
