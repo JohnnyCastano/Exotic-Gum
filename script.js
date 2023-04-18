91160162b578ca1b8e31bf6eadf21e496908dfdc
Johnny Castano <88343685+JohnnyCastano@users.noreply.github.com>
2023-04-18 08:46:23 -0500

    update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-/*Items that fade in*/
+/*Back to Top button*/
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
